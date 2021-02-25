739744a981cc0744e4f1a4248ae8c5780635341c
owzi_ <frolle@inf.ufpel.edu.br>
2021-02-25 07:14:24 -0300

    projeto react

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+// import { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p>App to serach GitHub users</p>
+      <p>Version: 0.9.9</p>
+    </div>
+  )
+}
+
+export default About 
