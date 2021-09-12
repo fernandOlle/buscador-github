b60cdd40dd39378d0a12a325b5b12a3f13619725
Fernando Ollé <55040050+fernandOlle@users.noreply.github.com>
2021-09-12 18:23:13 -0300

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <h1>About This App</h1>
-      <p>App to serach GitHub users</p>
+      <p>App to search GitHub users</p>
       <p>Version: 0.9.9</p>
     </div>
   )
